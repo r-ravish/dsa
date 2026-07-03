64d1560fc1fd5b5889f33f6576f94fa688358b41
r-ravish <www.ravishraheja@gmail.com>
2026-07-03 20:09:55 +0530

    Time: 3 ms (45.06%) | Memory: 9.7 MB (20.88%)

---
+++ 9-palindrome-number/palindrome-number.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        string s = to_string(x);
+        string check_s = "";
+
+        for(int i=size(s)-1; i>=0; i--){
+            check_s = check_s + s[i];
+        }
+
+        if(s==check_s){
+            return true;
+        }
+        else{
+            return false;
+        }
+
+    }
+};
