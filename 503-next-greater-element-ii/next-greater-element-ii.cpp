4ee61a88a7519c1fe1464f49a5a0a80989c81932
r-ravish <www.ravishraheja@gmail.com>
2026-07-09 03:18:37 +0530

    Time: 3 ms (74.23%) | Memory: 29.3 MB (11.23%)

--- 503-next-greater-element-ii/next-greater-element-ii.cpp
+++ 503-next-greater-element-ii/next-greater-element-ii.cpp
@@ -3,27 +3,19 @@ public:
     vector<int> nextGreaterElements(vector<int>& nums) {
         vector<int> ans;
         stack<int> st;
-        for(int i=nums.size()-1; i>=0; i--){
-            while(!st.empty() && st.top() <= nums[i]){
+
+        for(int i=2*nums.size()-1; i>=0; i--){
+            while(!st.empty() && st.top() <= nums[i%nums.size()]){
                 st.pop();
             }
 
-            if(st.empty()){
-                int j=1;
-                int temp = -1;
-                while((i+j)%nums.size() != i){
-                    if(nums[(i+j)%nums.size()] > nums[i]){
-                        temp = nums[(i+j)%nums.size()];
-                        break;
-                    }
-                    j++;
+            if(i<nums.size()){
+                if(st.empty()){
+                    ans.push_back(-1);
+                }else{
+                    ans.push_back(st.top());
                 }
-                ans.push_back(temp);
-            }else{
-                ans.push_back(st.top());
-            }
-
-            st.push(nums[i]);
+            }st.push(nums[i%nums.size()]);
         }
 
         reverse(ans.begin(), ans.end());
