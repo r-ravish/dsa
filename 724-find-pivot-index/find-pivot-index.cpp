9be064bc18985905153d58ac1384c5a160ad59ea
r-ravish <www.ravishraheja@gmail.com>
2026-07-08 20:37:36 +0530

    Time: 0 ms (100.00%) | Memory: 35.8 MB (55.86%)

---
+++ 724-find-pivot-index/find-pivot-index.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        for(int i=1; i<nums.size(); i++){
+            nums[i] = nums[i-1] + nums[i];
+        }
+
+        for(int i=0; i<nums.size(); i++){
+            if(i == 0){
+                if(nums[nums.size()-1]-nums[i] == 0){
+                    return i;
+                }
+            }else if(nums[i-1] == nums[nums.size()-1]-nums[i]){
+                return i;
+            }
+        }
+
+        return -1;
+    }
+};
