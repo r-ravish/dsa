052a7991134bebf4e6cee066eb86a3b07f4a0eeb
r-ravish <www.ravishraheja@gmail.com>
2026-07-10 01:43:14 +0530

    Time: 38 ms (75.46%) | Memory: 45.4 MB (56.39%)

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        unordered_map<int,int> mpp;
-        int count=0;
+        unordered_map<int, int> mp;
         int sum=0;
-        mpp[0] = 1;
+        int count=0;
+        mp[0] = 1;
         for(int i=0; i<nums.size(); i++){
-            sum+=nums[i];
-            if (mpp.contains (sum - k)){
-                count += mpp[sum - k];
-            } 
-            mpp[sum]++;
+            sum+=nums[i];            
+            if(mp.contains(sum-k)){
+                count+=mp[sum-k];
+            }
+            mp[sum]++;
+            
         }
 
         return count;
