006d80b86bd50da5266b6a55a81812acaeaa527d
r-ravish <www.ravishraheja@gmail.com>
2026-07-11 21:47:04 +0530

    Time: 1 ms (31.98%) | Memory: 43.4 MB (6.50%)

--- 58-length-of-last-word/length-of-last-word.java
+++ 58-length-of-last-word/length-of-last-word.java
@@ -1,7 +1,6 @@
 class Solution {
     public int lengthOfLastWord(String s) {
         String[] arr = s.split(" ");
-        String ans = arr[arr.length-1];
-        return ans.length();
+        return arr[arr.length-1].length();
     }
 }
