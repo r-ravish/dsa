0a109cf53056991108ad946a6de373a6f36437e9
r-ravish <www.ravishraheja@gmail.com>
2026-07-14 14:13:47 +0530

    Time: 39 ms (72.61%) | Memory: 45.4 MB (56.36%)

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -4,7 +4,7 @@ public:
         unordered_map<int,int> mp;
         int sum=0;
         int count=0;
-        mp[0]=1;
+        mp[0]=1; // Why do we need this? Try using [3] as an example with k = 3 
         for(int i=0; i<nums.size(); i++){
             sum+=nums[i];
             if(mp.contains(sum-k)){
