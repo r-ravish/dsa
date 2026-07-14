424499fefe26c1ec04c88e095afe7a67afc3cb8e
r-ravish <www.ravishraheja@gmail.com>
2026-07-14 18:02:39 +0530

    Time: 85 ms (19.07%) | Memory: 146.3 MB (56.45%)

---
+++ 523-continuous-subarray-sum/continuous-subarray-sum.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool checkSubarraySum(vector<int>& nums, int k) {
+        unordered_map<int,int> mp;
+        int sum=0;
+        mp[0] = -1;
+        for(int i=0; i<nums.size(); i++){
+            sum+=nums[i];
+            if(mp.contains(sum%k) && i-mp[sum%k] >= 2){
+                return true;
+            }
+
+            if(!mp.contains(sum%k)){
+                mp[sum%k] = i;
+            }
+        }
+
+        
+        return false;
+    }
+};
