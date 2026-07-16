ef5895bde9939d9174654f63357958d0bf2058a2
r-ravish <www.ravishraheja@gmail.com>
2026-07-16 17:09:08 +0530

    Time: 13 ms (39.49%) | Memory: 14.3 MB (20.28%)

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -3,12 +3,9 @@ public:
     int lengthOfLongestSubstring(string s) {
         unordered_set<char> st;
         int left = 0;
-        if(s.size()==0){
-            return 0;
-        }
-        int out = INT_MIN;
-
-        for(int i=0; i<s.size(); i++){
+        int out = 0;
+        int n=s.size();
+        for(int i=0; i<n; i++){
             while(st.contains(s[i])){
                 st.erase(s[left]);
                 left++;
