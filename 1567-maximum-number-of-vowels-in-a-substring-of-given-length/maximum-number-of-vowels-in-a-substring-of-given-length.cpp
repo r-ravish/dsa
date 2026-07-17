c9d9de43a4a49ac28fba2bac2faefda6ddcf657f
r-ravish <www.ravishraheja@gmail.com>
2026-07-17 15:37:24 +0530

    Time: 5 ms (64.07%) | Memory: 13.1 MB (60.17%)

--- 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.cpp
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.cpp
@@ -5,7 +5,6 @@ public:
         int cnt=0;
         int ans=0;
         for(int i=0; i<s.size(); i++){
-            
             if(s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u'){
                 cnt++;
             }
