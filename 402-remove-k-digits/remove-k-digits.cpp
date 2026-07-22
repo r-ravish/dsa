c1bf82551c9ac5f1d471766d1658412b12456766
r-ravish <www.ravishraheja@gmail.com>
2026-07-23 01:03:07 +0530

    Time: 3 ms (62.00%) | Memory: 12.6 MB (14.43%)

---
+++ 402-remove-k-digits/remove-k-digits.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    string removeKdigits(string num, int k) {
+        int n=num.size();
+        if(k==n){          // edge case
+            return "0";
+        }
+        string out = "";
+        stack<char> st;
+        for(int i=0; i<n; i++){
+            while(!st.empty() && st.top() > int(num[i]) && k>0){
+                st.pop();
+                k--;
+            }
+            st.push(int(num[i]));
+        }
+
+        while(k>0 && !st.empty()){
+            k--;
+            st.pop();
+        }
+
+        string res = "";
+        while(!st.empty()){
+            res+=st.top();
+            st.pop();
+        }
+
+        reverse(res.begin(), res.end());
+        
+        int s = res.size();
+        int z = 0;
+        while(res[z] == '0'){
+            z++;
+        }
+
+        int a = 0;
+        int count=0;
+        while(a != res.size()){
+            if(res[a] == '0'){
+                count++;
+            }else{
+                break;
+            }
+            a++;
+        }
+
+        if(count==res.size()){
+            return "0";
+        }
+
+        return res.substr(z,s);
+    }
+};
