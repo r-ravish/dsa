7b7e04800f53ba5b252fbfacc3b6ca7bfdb756f6
r-ravish <www.ravishraheja@gmail.com>
2026-07-26 09:06:56 +0530

    Time: 71 ms (36.84%) | Memory: 111.4 MB (23.27%)

--- 217-contains-duplicate/contains-duplicate.cpp
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -1,13 +1,11 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
+        int n = nums.size();
         unordered_map<int, int> mpp;
-        for(int i=0; i<nums.size(); i++){
+        for(int i=0; i<n; i++){
             mpp[nums[i]]++;
-        }
-
-        for(auto i : mpp){
-            if(i.second > 1){
+            if(mpp[nums[i]] > 1){
                 return true;
             }
         }
