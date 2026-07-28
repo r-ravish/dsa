92260c34bbf144b591a2e3bc07366020020da404
r-ravish <www.ravishraheja@gmail.com>
2026-07-28 23:42:27 +0530

    Time: 0 ms (100.00%) | Memory: 14.4 MB (62.81%)

---
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        vector<int> ans;
+        int left = 0;
+        int right = 0;
+
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
+
+        while(left < nums1.size() && right < nums2.size()){
+            if(nums1[left] == nums2[right]){
+                ans.push_back(nums1[left]);
+                left++;
+                right++;
+            }else if(nums1[left] < nums2[right]){
+                left++;
+            }else{
+                right++;
+            }
+        }
+
+        return ans;
+    }
+};
