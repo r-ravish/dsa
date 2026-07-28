f58e5b9a0a69d9c4ef29a5312ef222cccaa883e2
r-ravish <www.ravishraheja@gmail.com>
2026-07-29 01:28:14 +0530

    Time: 0 ms (100.00%) | Memory: 69.6 MB (32.30%)

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.cpp
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     int longestOnes(vector<int>& nums, int k) {
-        int zeros = 0;
         int left = 0;
+        int zeros = 0;
         int maxLen = 0;
         for(int i=0; i<nums.size(); i++){
             if(nums[i] == 0){
@@ -10,10 +10,14 @@ public:
             }
 
             if(zeros <= k){
-                maxLen = i-left+1;
-            }else{
-                if(nums[left] == 0) zeros--;
-                left+=1;
+                maxLen = max(maxLen, i-left+1);
+            }
+
+            while(zeros > k){
+                if(nums[left] == 0){
+                    zeros--;
+                }
+                left++;
             }
         }
 
