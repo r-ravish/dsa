7d162a49b6a8d874ba8e6635d576f5aacf206c64
r-ravish <www.ravishraheja@gmail.com>
2026-07-30 01:11:06 +0530

    Time: 19 ms (12.93%) | Memory: 11.2 MB (7.84%)

---
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        unordered_map<int, int> mpp;
+        int maxfreq = INT_MIN;
+        int left = 0;
+        int maxLen = 0;
+        
+        for(int i=0; i<s.size(); i++){
+            mpp[s[i]]++;
+            if(mpp[s[i]] > maxfreq){
+                maxfreq = mpp[s[i]];
+            }
+
+            if(i-left+1 - maxfreq <= k){
+                maxLen = max(maxLen, i-left+1);
+            }else{
+                while(i-left+1 - maxfreq > k){
+                    mpp[s[left]]--;
+                    if(mpp[s[left]] == 0){
+                        mpp.erase(s[left]);
+                    }
+                    left++;
+                }
+            }
+        }
+        return maxLen;
+    }
+};
