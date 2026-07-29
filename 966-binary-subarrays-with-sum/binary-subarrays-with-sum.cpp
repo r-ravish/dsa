1f1d895c4132351333fa1ea77edd322355691fa1
r-ravish <www.ravishraheja@gmail.com>
2026-07-30 02:34:49 +0530

    Time: 3 ms (54.86%) | Memory: 32.5 MB (83.38%)

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
@@ -1,18 +1,26 @@
 class Solution {
 public:
-    int numSubarraysWithSum(vector<int>& nums, int goal) {
-        unordered_map<int, int> mpp;
-        int maxLen = 0;
+    int goalCalculation(vector<int>& nums, int goal){
+        if(goal < 0) return 0;
         int sum=0;
-        mpp[0]=1;
+        int left = 0;
+        int ans = 0;
+
         for(int i=0; i<nums.size(); i++){
             sum+=nums[i];
-            if(mpp.contains(sum-goal)){
-                maxLen+=mpp[sum-goal];
+            while(sum > goal){
+                sum-=nums[left];
+                left++;
             }
-            mpp[sum]++;
+            ans+=(i-left+1);
         }
 
-        return maxLen;
+        return ans;
+    }
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
+        int a = goalCalculation(nums,goal);
+        int b = goalCalculation(nums,goal-1);
+
+        return a-b;
     }
 };
