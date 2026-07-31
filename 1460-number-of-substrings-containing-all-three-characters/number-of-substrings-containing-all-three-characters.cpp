0b7bda5a5725217967d7ac155f28966437a31f61
r-ravish <www.ravishraheja@gmail.com>
2026-08-01 02:39:32 +0530

    Time: 34 ms (14.90%) | Memory: 11.2 MB (28.47%)

---
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int mini(int a, int b, int c){
+        int temp = min(a,b);
+        int newTemp = min(temp,c);
+        return newTemp;
+    }
+
+    int numberOfSubstrings(string s) {
+        int ans = 0;
+        unordered_map<char, int> mpp = {{'a', -1}, {'b', -1}, {'c', -1}};
+        for(int i=0; i<s.size(); i++){
+            mpp[s[i]] = i;
+            if(mpp['a'] != -1 && mpp['b'] != -1 && mpp['c'] != -1){
+                int temp = mini(mpp['a'], mpp['b'], mpp['c']);
+                ans+=(temp+1);
+            }
+        }
+        return ans;
+    }
+};
