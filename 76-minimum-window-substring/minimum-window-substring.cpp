c5ce390a52f706773d806fa874027f360a1b3fd1
r-ravish <www.ravishraheja@gmail.com>
2026-08-02 02:01:16 +0530

    Time: 3 ms (78.10%) | Memory: 11.3 MB (94.64%)

---
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        unordered_map<char, int> mpp;
+
+        int left = 0;
+        int cnt=0;
+        int minLen = INT_MAX;
+        int startPoint = -1;
+        for(int i=0; i< t.size(); i++){
+            mpp[t[i]]++;
+        }
+        for(int i=0; i< s.size(); i++){
+            if(mpp[s[i]] > 0) cnt++;
+            mpp[s[i]]--;
+
+            while(cnt == t.size()){
+                if(i-left+1 <= minLen){
+                    minLen = i-left+1;
+                    startPoint = left;
+                }
+                mpp[s[left]]++;
+                if(mpp[s[left]] > 0){
+                    cnt--;
+                }
+                left++;
+            }
+
+        }
+
+        return startPoint == -1 ? "" : s.substr(startPoint, minLen);
+
+        
+    }
+};
