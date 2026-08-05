e6e06d04d9bf20df6bb878bbec704688fd8eee92
r-ravish <www.ravishraheja@gmail.com>
2026-08-05 07:10:52 +0530

    Time: 63 ms (7.52%) | Memory: 75.7 MB (20.52%)

--- 148-sort-list/sort-list.cpp
+++ 148-sort-list/sort-list.cpp
@@ -10,26 +10,64 @@
  */
 class Solution {
 public:
-    ListNode* sortList(ListNode* head) {
+    ListNode* merge(ListNode* left, ListNode* right){
+        ListNode* tail = new ListNode();
+        ListNode* dummy = tail;
+
+        while(left != NULL && right != NULL){
+            if(left->val <= right->val){
+                tail->next = left;
+                left = left->next;
+            }else{
+                tail->next=right;
+                right = right->next;
+            }
+
+            tail = tail->next;
+        }
+
+        while(left != NULL){
+            tail->next = left;
+            tail=tail->next;
+            left = left->next;
+        }
+
+        while(right != NULL){
+            tail->next = right;
+            tail=tail->next;
+            right = right->next;
+        }
+
+        return dummy->next;
+    }
+
+    ListNode* findMiddle(ListNode* head){
+        ListNode* mid = head;
+        ListNode* fast = head->next;
+        while(fast != NULL && fast->next != NULL){
+            mid = mid->next;
+            fast = fast->next->next;
+        }
+
+        return mid;
+    }
+
+    ListNode* mergeSort(ListNode* head){
         if(head == NULL || head->next == NULL) return head;
 
-        vector<int> arr;
-        ListNode* temp = head;
-        while(temp != NULL){
-            arr.push_back(temp->val);
-            temp=temp->next;
-        }
+        ListNode* middle = findMiddle(head);
 
-        sort(arr.begin(), arr.end());
+        ListNode* leftHead = head;
+        ListNode* rightHead = middle->next;
+        middle->next = NULL;
 
-        ListNode* newtemp = head;
-        int i=0;
-        while(newtemp != NULL && i<arr.size()){
-            newtemp->val = arr[i];
-            i++;
-            newtemp = newtemp->next;
-        }
+        leftHead = mergeSort(leftHead);
+        rightHead = mergeSort(rightHead);
 
-        return head;
+        return merge(leftHead, rightHead);
+    }
+
+    ListNode* sortList(ListNode* head) {
+        return mergeSort(head);
     }
 };
