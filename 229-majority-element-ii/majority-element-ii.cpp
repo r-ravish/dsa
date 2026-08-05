99c9a187884d4910b155fd049e4ec687bc1758d6
r-ravish <www.ravishraheja@gmail.com>
2026-08-05 06:36:11 +0530

    Time: 0 ms (100.00%) | Memory: 23.6 MB (70.51%)

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -2,17 +2,40 @@ class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
         int n = nums.size();
-        vector<int> ans;
-        unordered_map<int, int> mpp;
-        for(int i=0; i<nums.size(); i++){
-            mpp[nums[i]]++;
-        }
+        int cnt1 = 0;
+        int cnt2 = 0;
+        int el1 = INT_MIN;
+        int el2 = INT_MIN;
 
-        for(auto i : mpp){
-            if(i.second > n/3){
-                ans.push_back(i.first);
+        for(int i=0; i<n; i++){
+            if(cnt1 == 0 && nums[i] != el2){
+                cnt1=1;
+                el1=nums[i];
+            }else if(cnt2 == 0 && nums[i] != el1){
+                cnt2=1;
+                el2=nums[i];
+            }else if(nums[i] == el1){
+                cnt1++;
+            }else if(nums[i] == el2){
+                cnt2++;
+            }else{
+                cnt1--;
+                cnt2--;
             }
         }
+
+        vector<int> ans;
+        int mini = (int)n/3 + 1;
+        cnt1 = 0;
+        cnt2 = 0;
+        for(int i=0; i<n; i++){
+            if(el1 == nums[i]) cnt1++;
+            if(el2 == nums[i]) cnt2++;
+        }
+        if(cnt1 >= mini) ans.push_back(el1);
+        if(cnt2 >= mini) ans.push_back(el2);
+        
+        sort(ans.begin(), ans.end());
 
         return ans;
     }
