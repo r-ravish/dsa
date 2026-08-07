d4fac0a6144056bdeae62b267b0c1639208689df
r-ravish <www.ravishraheja@gmail.com>
2026-08-07 15:26:27 +0530

    Time: 4 ms (57.08%) | Memory: 11.1 MB (19.64%)

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -1,32 +1,26 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        int n = s.size();
-        unordered_map<int, int> mpp;
-        int maxfreq = INT_MIN;
         int left = 0;
+        unordered_map<char, int> mpp;
         int maxLen = 0;
-        
-        for(int i=0; i<n; i++){
+        int maxfreq = INT_MIN;
+        int temp = 0;
+        for(int i=0; i<s.size(); i++){
             mpp[s[i]]++;
             if(mpp[s[i]] > maxfreq){
                 maxfreq = mpp[s[i]];
             }
 
             if(i-left+1 - maxfreq <= k){
-                maxLen = max(maxLen, i-left+1);
-            }else{
-                // while(i-left+1 - maxfreq > k){
-                //     mpp[s[left]]--;
-                //     if(mpp[s[left]] == 0){
-                //         mpp.erase(s[left]); 
-                //     }
-                //     left++;
-                // }
+                maxLen = i-left+1;
+            }
+            while(i-left+1 - maxfreq > k){
                 mpp[s[left]]--;
                 left++;
             }
         }
+
         return maxLen;
     }
 };
