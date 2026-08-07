0502a8239a60065d69fba3211e85506c215d1d1d
r-ravish <www.ravishraheja@gmail.com>
2026-08-07 14:50:16 +0530

    Time: 8 ms (59.91%) | Memory: 71.4 MB (70.26%)

--- 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
@@ -1,28 +1,31 @@
 class Solution {
 public:
-    int lessthanK(vector<int>& nums, int k){
-        if(k<0) return 0;
-        int left = 0;
-        int odd = 0;
+    int niceSubarrays(vector<int>& nums, int k){
         int ans = 0;
+        int left = 0;
+        int oddCount = 0;
         for(int i=0; i<nums.size(); i++){
             if(nums[i]%2 != 0){
-                odd++;
+                oddCount++;
             }
-            while(odd > k){
-                if(nums[left]%2 != 0) odd --;
+
+            while(oddCount > k){
+                if(nums[left]%2 != 0){
+                    oddCount--;
+                }
                 left++;
             }
-            if(odd <= k){
+            if(oddCount <= k){
                 ans+=(i-left+1);
             }
         }
 
         return ans;
     }
+
     int numberOfSubarrays(vector<int>& nums, int k) {
-        int a = lessthanK(nums, k);
-        int b = lessthanK(nums, k-1);
+        int a = niceSubarrays(nums, k);
+        int b = niceSubarrays(nums, k-1);
 
         return a-b;
     }
