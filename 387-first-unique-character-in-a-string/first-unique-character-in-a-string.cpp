265edd93e9bc4929588c7e11fd7d632e4bbec25b
r-ravish <www.ravishraheja@gmail.com>
2026-08-09 23:07:51 +0530

    Time: 10 ms (51.97%) | Memory: 14.9 MB (39.69%)

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.cpp
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.cpp
@@ -2,10 +2,11 @@ class Solution {
 public:
     int firstUniqChar(string s) {
         unordered_map<char, int> mpp;
+        
         for(char c : s){
             mpp[c]++;
         }
-        
+
         for(int i=0; i<s.size(); i++){
             if(mpp[s[i]] == 1){
                 return i;
