84841cc99f7215a3090a839cea38c74b90304e87
r-ravish <www.ravishraheja@gmail.com>
2026-08-09 12:50:41 +0530

    Time: 379 ms (5.89%) | Memory: 204.8 MB (12.45%)

--- 455-assign-cookies/assign-cookies.cpp
+++ 455-assign-cookies/assign-cookies.cpp
@@ -1,16 +1,64 @@
 class Solution {
 public:
+
+    void merge(vector<int>& nums, int low, int mid, int high){
+        int left = low;
+        int right = mid+1;
+        vector<int> temp;
+
+        while(left <= mid && right <= high){
+            if(nums[left] <= nums[right]){
+                temp.push_back(nums[left]);
+                left++;
+            }else{
+                temp.push_back(nums[right]);
+                right++;
+            }
+        }
+
+        while(left <= mid){
+            temp.push_back(nums[left]);
+            left++;
+        }
+
+        while(right <= high){
+            temp.push_back(nums[right]);
+            right++;
+        }
+
+
+        for(int i=low; i<=high; i++){
+            nums[i] = temp[i - low];
+        }
+    }
+
+    void divide(vector<int>& nums, int low, int high){
+        if(low >= high){
+            return;
+        }
+        int mid = low + (high-low)/2;
+        divide(nums, low, mid);
+        divide(nums, mid+1, high);
+
+        merge(nums, low, mid, high);
+    }
+
+    void mergeSort(vector<int>& nums, int low, int high){
+        divide(nums, low, high);
+    }
+
     int findContentChildren(vector<int>& g, vector<int>& s) {
-        sort(g.begin(), g.end());
-        sort(s.begin(), s.end());
+        int n = g.size();
+        int m = s.size();
+        mergeSort(g, 0, n-1);
+        mergeSort(s, 0, m-1);
 
         int left = 0;
         int right = 0;
-        while(left < g.size() && right < s.size()){
+        while(left < n && right < m){
             if(s[right] >= g[left]){
                 left++;
                 right++;
-                // ans++;
             }else if(s[right] < g[left]){
                 right++;
             }
