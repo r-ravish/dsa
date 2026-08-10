6d60ba401d33962a5d100bc9095c7a75fa026ced
r-ravish <www.ravishraheja@gmail.com>
2026-08-11 02:10:56 +0530

    Time: 0 ms (100.00%) | Memory: 52.2 MB (83.73%)

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -1,19 +1,14 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        int n = nums.size();
-        int maxIndex = 0;
-
-        for(int i=0; i<n ; i++){
-            if(i > maxIndex) return false;
-            maxIndex = max(maxIndex, i+nums[i]);
-
-            if(maxIndex >= n-1){
-                return true;
+        int maxLen = 0;
+        for(int i=0; i<nums.size(); i++){
+            if(i>maxLen){
+                return false;
             }
-
+            maxLen = max(maxLen, i+nums[i]);
         }
 
-        return false;
+        return true;
     }
 };
