ebf3e44d65acc7d52d3508fe6e83742bd34009c7
r-ravish <www.ravishraheja@gmail.com>
2026-08-12 02:50:55 +0530

    Time: 63 ms (16.60%) | Memory: 31 MB (20.99%)

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -3,19 +3,23 @@ public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         sort(nums.begin(), nums.end());
 
-        set<vector<int>> ans;
+        vector<vector<int>> ans;
         int n = nums.size();
         for(int i=0; i<n; i++){
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+
             int j=i+1;
             int k=n-1;
 
             while(j<k){
                 if(nums[i] + nums[j] + nums[k] == 0){
                     vector<int> temp = {nums[i], nums[j], nums[k]};
-                    sort(temp.begin(), temp.end());
-                    ans.insert(temp);
+                    ans.push_back(temp);
                     j++;
                     k--;
+                    while(j<k && nums[j] == nums[j-1]) j++;
+                    while(j<k && nums[k] == nums[k+1]) k--;
                 }else if(nums[i] + nums[j] + nums[k] > 0){
                     k--;
                 }else{
@@ -24,11 +28,6 @@ public:
             }
         }
 
-        vector<vector<int>> out;
-        for(auto i : ans){
-            out.push_back(i);
-        }
-
-        return out;
+        return ans;
     }
 };
