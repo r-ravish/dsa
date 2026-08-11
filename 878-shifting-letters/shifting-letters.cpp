ead011f0dd7aad19de2d58eea1c27a261dde4858
r-ravish <www.ravishraheja@gmail.com>
2026-08-11 18:31:22 +0530

    Time: 8 ms (26.97%) | Memory: 73.3 MB (37.99%)

--- 878-shifting-letters/shifting-letters.cpp
+++ 878-shifting-letters/shifting-letters.cpp
@@ -2,9 +2,9 @@ class Solution {
 public:
     string shiftingLetters(string s, vector<int>& shifts) {
         for(int i=shifts.size()-2; i>=0; i--){
-            shifts[i] = (shifts[i] + shifts[i+1])%26;
+            shifts[i]=(shifts[i]+shifts[i+1])%26;
         }
-
+        
         string ans = "";
         for(int i=0; i<s.size(); i++){
             char ch = s[i];
