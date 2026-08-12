dc1da6afee12c5e40cc8b7e408829c74f8cda260
r-ravish <www.ravishraheja@gmail.com>
2026-08-13 01:18:41 +0530

    Time: 474 ms (72.58%) | Memory: 240 MB (74.19%)

---
+++ 493-reverse-pairs/reverse-pairs.cpp
@@ -0,0 +1,62 @@
+class Solution {
+public:
+    int cnt = 0;
+    void merge(vector<int>& nums, int low, int mid, int high){
+        vector<int> temp;
+        int left = low;
+        int right = mid+1;
+
+        while(left <= mid && right <= high){
+            if(nums[left] < nums[right]){
+                temp.push_back(nums[left]);
+                left++;
+            }else{
+                temp.push_back(nums[right]);
+                right++;
+            }
+        }
+
+        while(left <= mid){
+            temp.push_back(nums[left]);
+            left++;
+        }
+
+        while(right <= high){
+            temp.push_back(nums[right]);
+            right++;
+        }
+
+        for(int i=low; i<=high; i++){
+            nums[i] = temp[i-low];
+        }
+    }
+
+    void findCount(vector<int>& nums, int low, int mid, int high){
+        int right = mid+1;
+        for(int i=low; i<=mid; i++){
+            while(right <= high && nums[i] > 2LL*nums[right]){
+                right++;
+            }
+            cnt+=(right-(mid+1));
+        }
+    }
+
+    void divide(vector<int>& nums, int low, int high){
+        if(low>=high){
+            return;
+        }
+
+        int mid = low + (high-low)/2;
+        divide(nums, low, mid);
+        divide(nums, mid+1, high);
+        findCount(nums, low, mid, high);
+        merge(nums, low, mid, high);
+    }
+
+    int reversePairs(vector<int>& nums) {
+        int n = nums.size();
+        divide(nums, 0, n-1);
+
+        return cnt;
+    }
+};
