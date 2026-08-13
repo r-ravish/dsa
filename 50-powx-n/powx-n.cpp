137bff07b2339e4b8c991faf0d879e3b329c44fa
r-ravish <www.ravishraheja@gmail.com>
2026-08-14 00:54:09 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (13.18%)

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,24 +1,26 @@
 class Solution {
 public:
-    double powCount(double x, long long n){
-        if(n==0){
-            return 1;
-        }
-        double ans = powCount(x,n/2);
+    double calculatePower(double x, long long n){
+        if(n==0) return 1;
+        double ans = calculatePower(x,n/2);
         if(n%2 == 0){
             return ans*ans;
-        }else{
-            return ans*ans*x;
         }
+        
+        return x*ans*ans;
+
     }
 
     double myPow(double x, int n) {
-        long long N = n;
+        if(n==0) return 1;
+        if(x==1) return 1;
+
+        long long N=n;
         if(n<0){
-            double a = powCount(x,-N);
-            return 1/a;
+            double out = calculatePower(x, -N);
+            return 1/out;
         }
 
-        return powCount(x,N);
+        return calculatePower(x, N);
     }
 };
