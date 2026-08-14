925390c2ee84a50cf7a1fefd06986f609395c705
r-ravish <www.ravishraheja@gmail.com>
2026-08-15 03:36:50 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (82.74%)

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
-    vector<vector<int>> ans;
-    void printSubsets(int ind, vector<int> temp, vector<int> nums, int n){
+    void printSubsets(int ind, vector<int>& temp, vector<int>& nums, int n, vector<vector<int>>& ans){
         if(ind >= n){
             ans.push_back(temp);
             return;
         }
 
         temp.push_back(nums[ind]);
-        printSubsets(ind+1, temp, nums, n);
+        printSubsets(ind+1, temp, nums, n, ans);
         temp.pop_back();
-        printSubsets(ind+1, temp, nums, n);
+        printSubsets(ind+1, temp, nums, n, ans);
     }
 
     vector<vector<int>> subsets(vector<int>& nums) {
         int n = nums.size();
+        vector<vector<int>> ans;
         vector<int> temp;
-        printSubsets(0, temp, nums, n);
+        printSubsets(0, temp, nums, n, ans);
 
         return ans;
     }
