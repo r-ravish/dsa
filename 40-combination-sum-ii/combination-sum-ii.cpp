2c61a35a65e754b6b6dc79f5b1e7354372ea310e
r-ravish <www.ravishraheja@gmail.com>
2026-08-16 01:43:15 +0530

    Time: 0 ms (100.00%) | Memory: 13.9 MB (85.26%)

--- 40-combination-sum-ii/combination-sum-ii.cpp
+++ 40-combination-sum-ii/combination-sum-ii.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
-    void printCombinationSum(int ind, vector<int>& temp, vector<int>& nums, vector<vector<int>>& ans, int sum, int target, int n){
-        if(sum==target){
+    void printCombinationSum(int ind, vector<int>& temp, vector<int>& nums, vector<vector<int>>& ans, int target, int n){
+        if(target == 0){
             ans.push_back(temp);
             return;
         }
 
         for(int i=ind; i<n; i++){
             if(i>ind && nums[i] == nums[i-1]) continue;
-            if(nums[i] > target-sum) break;
+            if(nums[i] > target) break;
 
             temp.push_back(nums[i]);
-            sum+=nums[i];
-            printCombinationSum(i+1, temp, nums, ans, sum, target, n);
-            sum-=nums[i];
+            printCombinationSum(i+1, temp, nums, ans, target-nums[i], n);
             temp.pop_back();
         }
     } 
@@ -22,12 +20,11 @@ public:
         sort(candidates.begin(), candidates.end());
 
         int n = candidates.size();
-        int sum=0;
 
         vector<int> temp;
         vector<vector<int>> ans;
 
-        printCombinationSum(0, temp, candidates, ans, sum, target, n);
+        printCombinationSum(0, temp, candidates, ans, target, n);
 
         return ans;
     }
