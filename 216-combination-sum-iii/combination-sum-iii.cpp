f19e2de74ed26b90e5b7b22a241f67ffce7779a4
r-ravish <www.ravishraheja@gmail.com>
2026-08-16 17:03:26 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (82.96%)

--- 216-combination-sum-iii/combination-sum-iii.cpp
+++ 216-combination-sum-iii/combination-sum-iii.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
-    void findCombinationSum(int ind, vector<int>& temp, vector<int>& nums, vector<vector<int>>& ans, int target, int size, int& k){
+    void findCombinationSum(int ind, vector<int>& temp, vector<vector<int>>& ans, int target, int size, int& k){
         if(target==0){
             if(temp.size() == k) ans.push_back(temp);
             return;
         }
 
-        for(int i=ind; i<size; i++){
-            if(nums[i] > target) break;
-            temp.push_back(nums[i]);
-            findCombinationSum(i+1, temp, nums, ans, target-nums[i], size, k);
+        for(int i=ind; i<=9; i++){
+            temp.push_back(i);
+            findCombinationSum(i+1, temp, ans, target-i, size, k);
             temp.pop_back();
         }
     }
@@ -18,12 +17,12 @@ public:
     vector<vector<int>> combinationSum3(int k, int n) {
         vector<int> temp;
         int size = 9;
-        vector<int> nums = {1,2,3,4,5,6,7,8,9};
+        // vector<int> nums = {1,2,3,4,5,6,7,8,9};
 
         vector<vector<int>> ans;
         int sum = 0;
 
-        findCombinationSum(0, temp, nums, ans, n, size, k);
+        findCombinationSum(1, temp, ans, n, size, k);
 
         return ans;
     }
