3105f2896b4ddacba5ec9ce872d9d9d9798266db
r-ravish <www.ravishraheja@gmail.com>
2026-08-18 18:05:18 +0530

    Time: 0 ms (100.00%) | Memory: 71.7 MB (53.20%)

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int maxi = INT_MIN;
-        int sum=0;
-        for(int i=0; i<nums.size(); i++){
+        int ans = INT_MIN;
+        int sum = 0;
+        int n = nums.size();
+        for(int i=0; i<n; i++){
             sum+=nums[i];
-            maxi = max(sum, maxi);
-            if(sum < 0) sum = 0;
+            ans = max(ans, sum);
+            if(sum < 0) sum=0;
         }
-
-        return maxi;
+        return ans;
     }
 };
