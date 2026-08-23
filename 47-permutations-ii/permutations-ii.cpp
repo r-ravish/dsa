66fb585c4f82a21807d1a0b1ff78d5641757a8d3
r-ravish <www.ravishraheja@gmail.com>
2026-08-24 01:59:03 +0530

    Time: 3 ms (63.02%) | Memory: 15.1 MB (16.78%)

--- 47-permutations-ii/permutations-ii.cpp
+++ 47-permutations-ii/permutations-ii.cpp
@@ -1,41 +1,30 @@
 class Solution {
 public:
-    void genPermutations(vector<int>& nums, vector<int>& temp, int n, unordered_map<int,int>& mpp, vector<vector<int>>& ans){
-        if(temp.size() == n){
-            ans.push_back(temp);
+    void getPermutations(int ind, vector<int>& nums, vector<vector<int>>& ans, int n){
+        if(ind >= n){
+            ans.push_back(nums);
             return;
         }
 
         unordered_set<int> used;
 
-
-        for(int i=0; i<n; i++){
+        for(int i=ind; i<n; i++){
             if(used.count(nums[i])){
                 continue;
             }
+            used.insert(nums[i]);
+            swap(nums[ind], nums[i]);
             
-            if(mpp[nums[i]] > 0){
-                temp.push_back(nums[i]);
-                mpp[nums[i]]--;
-                used.insert(nums[i]);
-                genPermutations(nums, temp, n, mpp, ans);
-                mpp[nums[i]]++;
-                temp.pop_back();
-            }
+            getPermutations(ind+1, nums, ans, n);
+            swap(nums[ind], nums[i]);
         }
     }
 
     vector<vector<int>> permuteUnique(vector<int>& nums) {
         int n = nums.size();
-        unordered_map<int,int> mpp;
-        vector<int> temp;
         vector<vector<int>> ans;
 
-        for(int i=0; i<nums.size(); i++){
-            mpp[nums[i]]++;
-        }
-
-        genPermutations(nums, temp, n, mpp, ans);
+        getPermutations(0, nums, ans, n);
 
         return ans;
     }
