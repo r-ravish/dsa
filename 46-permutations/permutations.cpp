cbd29713f3d77db6436ec2bf3008a5172b4c5719
r-ravish <www.ravishraheja@gmail.com>
2026-08-24 01:17:16 +0530

    Time: 0 ms (100.00%) | Memory: 10.4 MB (89.06%)

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -1,27 +1,23 @@
 class Solution {
 public:
-    void genPermutations(vector<int>& nums, vector<int> temp, vector<vector<int>>& ans, unordered_map<int,bool>& mpp){
-        if(temp.size() == nums.size()){
-            ans.push_back(temp);
-            return;
+    void genPermutation(int ind, vector<int>& nums, int n, vector<vector<int>>& ans){
+        if(ind >= n){
+            ans.push_back(nums);
         }
 
-        for(int i=0; i<nums.size(); i++){
-            if(mpp.contains(nums[i])) continue;
-
-            temp.push_back(nums[i]);
-            mpp[nums[i]] = true;
-            genPermutations(nums, temp, ans, mpp);
-            temp.pop_back();
-            mpp.erase(nums[i]);
+        for(int i=ind; i<n; i++){
+            swap(nums[ind], nums[i]);
+            genPermutation(ind+1, nums, n, ans);
+            swap(nums[ind], nums[i]);
         }
+
     }
 
     vector<vector<int>> permute(vector<int>& nums) {
-        vector<int> temp; 
+        int n = nums.size();
         vector<vector<int>> ans;
-        unordered_map<int,bool> mpp;
-        genPermutations(nums, temp, ans, mpp);
+
+        genPermutation(0, nums, n, ans);
 
         return ans;
     }
