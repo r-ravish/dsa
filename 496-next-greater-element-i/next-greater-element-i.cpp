414a9d7fbd7da9fcc462757b8916eae6bbbd26b2
r-ravish <www.ravishraheja@gmail.com>
2026-09-16 00:34:34 +0530

    Time: 3 ms (41.25%) | Memory: 12.8 MB (38.47%)

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -1,33 +1,36 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        unordered_map<int, int> out;
+        unordered_map<int, int> mpp;
         stack<int> st;
         vector<int> ans;
 
-        for(int i=nums2.size()-1; i>=0; i--){
+        int n = nums2.size();
+
+        for(int i=n-1; i>=0; i--){
             if(st.empty()){
-                out[nums2[i]] = -1;
+                mpp[nums2[i]] = -1;
                 st.push(nums2[i]);
             }else{
                 while(!st.empty() && st.top() < nums2[i]){
                     st.pop();
                 }
-                
+
                 if(st.empty()){
-                    out[nums2[i]] = -1;
+                    mpp[nums2[i]] = -1;
                 }else{
-                    out[nums2[i]] = st.top();
+                    mpp[nums2[i]] = st.top();
                 }
-                    
+
                 st.push(nums2[i]);
             }
         }
 
-        for (int i = 0; i < nums1.size(); i++) {
-            ans.push_back(out[nums1[i]]);
+        for(int i=0; i<nums1.size(); i++){
+            ans.push_back(mpp[nums1[i]]);
         }
 
         return ans;
+
     }
 };
